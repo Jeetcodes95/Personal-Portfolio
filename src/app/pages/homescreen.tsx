d5b4bd9362ab373c6f4699525662a2d7e36c44e1
Jeetendra Kumar Barman <jeet@192.168.1.31>
2024-06-30 14:39:19 +0530

    Set resume to be downloaded

--- src/app/pages/homescreen.tsx
+++ src/app/pages/homescreen.tsx
@@ -61,7 +61,7 @@ export default function HomeScreen() {
           <div className="absolute inset-0 w-full h-full bg-black [mask-image:radial-gradient(450px_200px_at_top,transparent_20%,white)]"></div>
         </div>
 
-        <a href="/Resume_JeetendraKumarBarman.pdf" target="_blank" >
+        <a href="/Resume_JeetendraKumarBarman.pdf" download="Resume_JeetendraKumarBarman.pdf" target="_blank" >
         <div className="relative inline-flex h-10 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
                 <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
                 <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
